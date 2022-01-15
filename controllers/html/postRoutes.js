40dd3021265ec56073703640a06973cd011fbd7f
Michael Rodriguez <mikeyrod22@gmail.com>
2022-01-15 14:39:06 -0600

    add route to render individual post pages

--- controllers/html/postRoutes.js
+++ controllers/html/postRoutes.js
@@ -40,10 +40,37 @@ router.get("/", (req, res) => {
 })
 
 // render single post page
-// router.get("/post/:id", (req, res) => {
-//   Post.findOne({
-//     where: { id: req.params.id },
-//   })
-// })
+router.get("/:id", (req, res) => {
+  Post.findOne({
+    where: { id: req.params.id },
+    attributes: ["id", "post_title", "post_body", "created_at"],
+    include: [
+      {
+        model: Comment,
+        attributes: ["id", "comment_body", "post_id", "user_id", "created_at"],
+        include: {
+          model: User,
+          attributes: ["username"],
+        },
+      },
+      {
+        model: User,
+        attributes: ["username"],
+      },
+    ],
+  })
+    .then(dbPostData => {
+      return !dbPostData
+        ? res.status(404).json({ message: "No post found with this id" })
+        : res.render("single-post", {
+            post: dbPostData.get({ plain: true }),
+            loggedIn: req.session.loggedIn,
+          })
+    })
+    .catch(err => {
+      console.log(err)
+      return res.status(500).json(err)
+    })
+})
 
 module.exports = router
