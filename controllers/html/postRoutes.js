c8e7a245ce3eea7735437ac7c268b41bf6d49b62
Michael Rodriguez <mikeyrod22@gmail.com>
2022-01-15 02:12:49 -0600

    add post-preview partial draft

--- controllers/html/postRoutes.js
+++ controllers/html/postRoutes.js
@@ -1,9 +1,31 @@
 const router = require("express").Router()
+const { Post, User, Comment } = require("../../models")
 
-router.get("/", (req, res) =>
-  res.render("posts", {
-    loggedIn: req.session.loggedIn,
+router.get("/", (req, res) => {
+  Post.findAll({
+    attributes: ["id", "post_title", "post_body", "created_at"],
+    include: [
+      {
+        model: Comment,
+        attributes: ["id", "comment_body", "post_id", "user_id", "created_at"],
+        include: {
+          model: User,
+          attributes: ["username"],
+        },
+      },
+      {
+        model: User,
+        attributes: ["username"],
+      },
+    ],
+  }).then(dbPostData => {
+    const posts = dbPostData.map(post => post.get({ plain: true }))
+    console.log(posts)
+    res.render("posts", {
+      posts,
+      loggedIn: req.session.loggedIn,
+    })
   })
-)
+})
 
 module.exports = router
