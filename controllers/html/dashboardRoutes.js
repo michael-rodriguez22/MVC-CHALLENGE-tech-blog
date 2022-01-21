8c16e2d5db18fe0823a2adffab1285aed1e30361
Michael Rodriguez <mikeyrod22@gmail.com>
2022-01-21 16:28:29 -0600

    add dashboard header

--- controllers/html/dashboardRoutes.js
+++ controllers/html/dashboardRoutes.js
@@ -1,5 +1,12 @@
 const router = require("express").Router()
 
-router.get("/", (req, res) => res.render("dashboard", { loggedIn: true }))
+router.get("/", (req, res) =>
+  res.render("dashboard", {
+    loggedIn: true,
+    dashboard: true,
+    username: req.session.username,
+    sessionUserId: req.session.user_id,
+  })
+)
 
 module.exports = router
